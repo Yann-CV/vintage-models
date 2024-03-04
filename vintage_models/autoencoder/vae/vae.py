7235383458a7197d69fb4ca6885f7ec83f277ec6
Yann-CV <ychene@scortex.io>
2024-03-04 08:30:25 +0100

    clean import

--- vintage_models/autoencoder/vae/vae.py
+++ vintage_models/autoencoder/vae/vae.py
@@ -1,7 +1,6 @@
 from functools import partial
 
-import torch
-from torch import Tensor
+from torch import Tensor, reshape, tensor, normal, device as torch_device, randn, log
 from torch.nn import Module, Linear, Tanh, Softplus
 from torch.nn.functional import binary_cross_entropy, sigmoid
 
@@ -18,7 +17,7 @@ class VaeEncoder(Module):
     ) -> None:
         super().__init__()
         in_size = image_width * image_height
-        self.to_vector = partial(torch.reshape, shape=(-1, in_size))
+        self.to_vector = partial(reshape, shape=(-1, in_size))
 
         self.linear_with_tanh = LinearWithActivation(
             in_size=in_size, out_size=hidden_size, activation_layer=Tanh()
@@ -28,7 +27,7 @@ class VaeEncoder(Module):
         )
         self.mean_linear = Linear(hidden_size, latent_size)
 
-        self._epsilon = torch.tensor(1e-6)
+        self._epsilon = tensor(1e-6)
 
     def compute_mean_and_std(self, x: Tensor) -> tuple[Tensor, Tensor]:
         if x.size(-3) != 1:
@@ -43,9 +42,7 @@ class VaeEncoder(Module):
 
     def forward(self, x: Tensor) -> Tensor:
         mean, std = self.compute_mean_and_std(x)
-        return mean + std * torch.normal(
-            mean=0, std=1, size=std.size(), device=std.device
-        )
+        return mean + std * normal(mean=0, std=1, size=std.size(), device=std.device)
 
 
 class VaeDecoder(Module):
@@ -67,15 +64,15 @@ class VaeDecoder(Module):
         )
         self.mean_linear = Linear(hidden_size, out_size)
 
-        self.to_image = partial(torch.reshape, shape=(-1, 1, image_width, image_height))
-        self._epsilon = torch.tensor(1e-6)
+        self.to_image = partial(reshape, shape=(-1, 1, image_width, image_height))
+        self._epsilon = tensor(1e-6)
 
     def forward(self, x: Tensor) -> Tensor:
         activated = self.linear_with_tanh(x)
         mean = self.mean_linear(activated)
         std = self._epsilon + 10 ** self.std_linear(activated)
 
-        random_value = torch.normal(mean=0, std=1, size=std.size(), device=std.device)
+        random_value = normal(mean=0, std=1, size=std.size(), device=std.device)
         sampled = sigmoid(mean + std * random_value)
 
         return self.to_image(sampled)
@@ -88,14 +85,14 @@ class Vae(Module):
         image_height: int,
         hidden_size: int,
         latent_size: int,
-        device: str | torch.device | int = "cpu",
+        device: str | torch_device | int = "cpu",
     ) -> None:
         super().__init__()
         self.latent_size = latent_size
         self.image_width = image_width
         self.image_height = image_height
         self.hidden_size = hidden_size
-        self.device = torch.device(device)
+        self.device = torch_device(device)
         self.encoder = VaeEncoder(
             image_width, image_height, hidden_size, latent_size
         ).to(device)
@@ -118,12 +115,12 @@ class Vae(Module):
         )
 
         mean, std = self.encoder.compute_mean_and_std(x)
-        kl_div = 0.5 * (mean.pow(2) + std - torch.log(std) - 1)
+        kl_div = 0.5 * (mean.pow(2) + std - log(std) - 1)
 
         return reconstruction_loss.sum(dim=1).mean() + kl_div.sum(dim=1).mean()
 
     def generate(self, n: int) -> Tensor:
-        return self.decoder(torch.randn(n, self.latent_size, device=self.device))
+        return self.decoder(randn(n, self.latent_size, device=self.device))
 
     def __str__(self) -> str:
         return (
