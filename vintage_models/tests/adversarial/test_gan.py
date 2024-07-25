04279de9f17d11facfabe5562f2394eed4a43e94
Yann-CV <ychene@scortex.io>
2024-07-25 18:15:38 +0200

    fix linting + fix pytests

--- vintage_models/tests/adversarial/test_gan.py
+++ vintage_models/tests/adversarial/test_gan.py
@@ -5,8 +5,10 @@ from vintage_models.adversarial.gan.gan import (
     GanDiscriminator,
     GanGenerator,
     Gan,
-    GanLosses,
 )
+
+
+GPU_NOT_AVAILABLE = not torch.cuda.is_available()
 
 
 @pytest.fixture
@@ -16,11 +18,11 @@ def input():
 
 @pytest.fixture
 def latent_input():
-    return torch.randint(0, 256, (2, 1, 2)).float() / 255
+    return torch.randint(0, 256, (2, 2)).float() / 255
 
 
 class TestGanGenerator:
-    generator = GanGenerator(28, 28, 100, 2)
+    generator = GanGenerator(out_width=28, out_height=28, input_size=2, latent_size=100)
 
     def test_simple(self, latent_input):
         output = self.generator(latent_input)
@@ -29,6 +31,11 @@ class TestGanGenerator:
     def test_wrong_input_size(self, latent_input):
         with pytest.raises(RuntimeError):
             self.generator(torch.zeros(2, 1, 3))
+
+    @pytest.mark.skipif(GPU_NOT_AVAILABLE, reason="No gpu available")
+    def test_gpu_usage(self, latent_input):
+        self.generator.to("cuda")
+        self.generator(latent_input.to("cuda"))
 
 
 class TestGanDiscriminator:
@@ -42,23 +49,28 @@ class TestGanDiscriminator:
         with pytest.raises(RuntimeError):
             self.discriminator(torch.zeros(4, 1, 28, 29))
 
+    @pytest.mark.skipif(GPU_NOT_AVAILABLE, reason="No gpu available")
+    def test_gpu_usage(self, input):
+        self.discriminator.to("cuda")
+        self.discriminator(input.to("cuda"))
+
 
 class TestGan:
-    gan = Gan(28, 28, 100, 2, 5)
+    gan = Gan(
+        image_width=28,
+        image_height=28,
+        generator_input_size=100,
+        generator_latent_size=500,
+        discriminator_hidden_size=5,
+        discriminator_maxout_depth=2,
+    )
 
     def test_simple(self, input):
         output = self.gan(input)
         assert output.shape == (2, 1)
         assert torch.all((output >= 0) & (output <= 1))
 
-    def test_generate(self, input):
-        generated = self.gan.generate(2)
-        assert generated.shape == (2, 1, 28, 28)
-
-    def test_loss(self, input):
-        losses = self.gan.loss(input)
-        generator_loss = losses.generator_loss.item()
-        discriminator_loss = losses.discriminator_loss.item()
-        assert isinstance(losses, GanLosses)
-        assert isinstance(generator_loss, float)
-        assert isinstance(discriminator_loss, float)
+    @pytest.mark.skipif(GPU_NOT_AVAILABLE, reason="No gpu available")
+    def test_gpu_usage(self, input):
+        self.gan.to("cuda")
+        self.gan(input.to("cuda"))
